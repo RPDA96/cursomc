7979cc55ebc3c1018b3e8dabea66b5b04dfbce17
Rodrigo Pereira <rodrigopda@live.com>
2019-12-30 13:54:48 -0300

    validação e DTO para Categorias

--- src/main/java/br/com/rodrigopereira/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/rodrigopereira/cursomc/services/CategoriaService.java
@@ -1,12 +1,17 @@
 package br.com.rodrigopereira.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.rodrigopereira.cursomc.domain.Categoria;
+import br.com.rodrigopereira.cursomc.dto.CategoriaDTO;
 import br.com.rodrigopereira.cursomc.repositories.CategoriaRepository;
 import br.com.rodrigopereira.cursomc.services.exceptions.DataIntegrityException;
 import br.com.rodrigopereira.cursomc.services.exceptions.ObjectNotFoundException;
@@ -40,5 +45,18 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
